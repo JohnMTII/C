76dc2739f3ad6b96d180481c16472d2d22d86833
JohnMTII <139885927+JohnMTII@users.noreply.github.com>
2023-07-30 02:14:41 -0700

    scrpit_1.c

--- scrpit_1.c
+++ scrpit_1.c
@@ -12,18 +12,10 @@ typedef struct {
 void InitList(SqList L){
     L.length=0;
 }
-//顺序表长度再分配
-void ListInsert(SqList L,int  i,int e){
-    for(int j=L.length;j>=i;j--)
-        L.data[j]=L.data[j-1];//转储
-    L.data[i-1]=e;          //在位置i存入e
-    L.length++;             //长度加一
-}
 
 int main(){
     SqList L;               //创建一个顺序表(申明一个顺序表)
     InitList(L);            //初始化顺序表
-//    ListInsert(L,3,3);          //动态转储
     for (int i = 0; i < MaxSize; i++) {
         printf("data[%d]=%d\n",i,L.data[i]);
     }
